8d31b7e2a7b536c747ea5c1c67d34409b7447336
Itai Weisman <iweisman@infinidat.com>
2018-06-10 20:44:05 +0300

    modified:   sg_delete.py 	new file:   t.py

--- sg_delete.py
+++ sg_delete.py
@@ -2,10 +2,21 @@ from infinini import ibox_login
 def sg_delete(sg_name):
 	system=ibox_login("ibox1499")
 	sg=system.cons_groups.find(system.cons_groups.fields.name == sg_name).to_list()
-	for sgn in sg:
-		if (sgn.get_children().to_list()):
-			print "Cannot delete SG {} which has snapshots".format(sgn)
-		else:
-			sgn.delete(delete_members=True)
+	if sg:
+		for sgn in sg:
+			if sgn.get_type() != 'SNAPSHOT':
+				print "{} is not an Snap Group".format(sg_name)	
+				return False
+
+			members=sgn.get_members().to_list()
+			for member in members:	
+				print "member is {} has_children is {}".format(member,member.has_children())
+				if member.has_children():
+					print "{} - has snapshot(s)!!! cannot delete".format(member.get_name())
+					return False
+			print "Deleting {}".format(sg_name)
+			return sgn.delete(delete_members=True)
+	else:
+		print "{} - Cannot be found".format(sg_name)
 
 
