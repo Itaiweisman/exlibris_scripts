340dc961ac46a8513ebe33016cfdcdaa5bd6b241
Itai Weisman <iweisman@infinidat.com>
2018-07-26 10:09:51 +0300

    modified:   snap_delete.py

--- snap_delete.py
+++ snap_delete.py
@@ -4,10 +4,15 @@ def snap_delete(snap_name):
                 system=ibox_login("ibox1499")
                 snap=system.volumes.find(system.volumes.fields.name == snap_name,type='SNAPSHOT').to_list()
                 if snap:
-			return snap[0].delete()
+			snapn=snap[0]
+			if snapn.get_family_master().get_cons_group():
+				print "Snapshot master volume is in a CG ";
+				return False,"Master is in CG";
+			return snapn.delete()
                 else:
                         print "{} - Cannot be found".format(snap_name)
 			return False, "Cannot be found"
         except Exception as E:
                 print "caught exception {}".format(E)
                 return False,E
+snap_delete('s1')
